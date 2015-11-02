20e4465c533ab414ee07bd1ef4407f11ef81d3ee
Ionică Bizău <bizauionica@gmail.com>
2015-11-02 10:49:55 +0200

    Added examples

---
+++ example/index.js
@@ -0,0 +1,39 @@
+// Dependencies
+var JsonToMarkdown = require("../lib");
+
+console.log(JsonToMarkdown([
+    { h1: "JSON To Markdown" }
+  , { blockquote: "A JSON to Markdown converter." }
+  , { img: [
+        { title: "Some image", source: "https://example.com/some-image.png" }
+      , { title: "Another image", source: "https://example.com/some-image1.png" }
+      , { title: "Yet another image", source: "https://example.com/some-image2.png" }
+      ]
+    }
+  , { h2: "Features" }
+  , { ul: [
+        "Easy to use"
+      , "You can programatically generate Markdown content"
+      , "..."
+      ]
+    }
+  , { h2: "How to contribute" }
+  , { ol: [
+        "Fork the project"
+      , "Create your branch"
+      , "Raise a pull request"
+      ]
+    }
+  , { h2: "Code blocks" }
+  , { p: "Below you can see a code block example." }
+  , { "code": {
+        language: "js"
+      , content: [
+          "function sum (a, b) {"
+        , "   return a + b;"
+        , "}"
+        , "sum(1, 2);"
+        ]
+      }
+    }
+]));
