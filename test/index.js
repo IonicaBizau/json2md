3d60c730746802a3e6c931dd25ed1ea35826e2e1
Ionică Bizău <bizauionica@gmail.com>
2015-11-02 10:51:31 +0200

    Call the callbacks

--- test/index.js
+++ test/index.js
@@ -10,11 +10,13 @@ it("should support headings", function (cb) {
     Assert.equal(JsonToMarkdown({ h4: "Heading 4" }), "#### Heading 4");
     Assert.equal(JsonToMarkdown({ h5: "Heading 5" }), "##### Heading 5");
     Assert.equal(JsonToMarkdown({ h6: "Heading 6" }), "###### Heading 6");
+    cb();
 });
 
 // Blockquote
 it("should support blockquotes", function (cb) {
     Assert.equal(JsonToMarkdown({ blockquote: "Some content" }), "> Some content");
+    cb();
 });
 
 // Images
@@ -25,6 +27,7 @@ it("should support images", function (cb) {
           , title: "title"
         }
     }), "![title](source)");
+    cb();
 });
 
 // Unordered lists
@@ -35,6 +38,7 @@ it("should support unordered lists", function (cb) {
           , "item 2"
         ]
     }), " - item 1\n - item 2");
+    cb();
 });
 
 // Ordered lists
@@ -45,6 +49,7 @@ it("should support ordered lists", function (cb) {
           , "item 2"
         ]
     }), " 1. item 1\n 2. item 2");
+    cb();
 });
 
 // Code blocks
@@ -60,6 +65,7 @@ it("should support code blocks", function (cb) {
             ]
         }
     }), "```js\nfunction sum (a, b) {\n   return a + b;\n}\nsum(1, 2);");
+    cb();
 });
 
 // Paragraphs
@@ -70,4 +76,5 @@ it("should support paragraphs", function (cb) {
           , "Paragraphs"
         ]
     }), "Two\nParagraphs");
+    cb();
 });
