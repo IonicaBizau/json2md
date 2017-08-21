79f9fa7f0f09e7ae7de4d7e1b1a3e2723a43b649
Ionică Bizău <bizauionica@gmail.com>
2017-08-21 10:04:10 +0300

    Fix tests

--- test/index.js
+++ test/index.js
@@ -7,22 +7,22 @@ tester.describe("json2md", test => {
     test.it("should support headings", function(cb) {
         test.expect(json2md({
             h1: "Heading 1"
-        })).toBe("# Heading 1");
+        })).toBe("# Heading 1\n");
         test.expect(json2md({
             h2: "Heading 2"
-        })).toBe("## Heading 2");
+        })).toBe("## Heading 2\n");
         test.expect(json2md({
             h3: "Heading 3"
-        })).toBe("### Heading 3");
+        })).toBe("### Heading 3\n");
         test.expect(json2md({
             h4: "Heading 4"
-        })).toBe("#### Heading 4");
+        })).toBe("#### Heading 4\n");
         test.expect(json2md({
             h5: "Heading 5"
-        })).toBe("##### Heading 5");
+        })).toBe("##### Heading 5\n");
         test.expect(json2md({
             h6: "Heading 6"
-        })).toBe("###### Heading 6");
+        })).toBe("###### Heading 6\n");
         cb();
     });
 
@@ -51,7 +51,7 @@ tester.describe("json2md", test => {
             ul: [
                 "item 1", "item 2"
             ]
-        })).toBe("\n - item 1\n - item 2");
+        })).toBe("\n - item 1\n - item 2\n");
         cb();
     });
 
@@ -60,7 +60,7 @@ tester.describe("json2md", test => {
             ul: [
                 "<em>item 1</em>", "<bold>item 2</bold>"
             ]
-        })).toBe("\n - *item 1*\n - **item 2**");
+        })).toBe("\n - *item 1*\n - **item 2**\n");
         cb();
     });
 
@@ -70,7 +70,7 @@ tester.describe("json2md", test => {
             ol: [
                 "item 1", "item 2"
             ]
-        })).toBe("\n 1. item 1\n 2. item 2");
+        })).toBe("\n 1. item 1\n 2. item 2\n");
         cb();
     });
 
@@ -80,10 +80,13 @@ tester.describe("json2md", test => {
             code: {
                 language: "js",
                 content: [
-                    "function sum (a, b) {", "   return a + b;", "}", "sum(1, 2);"
+                    "function sum (a, b) {",
+                    "   return a + b;",
+                    "}",
+                    "sum(1, 2);"
                 ]
             }
-        })).toBe("```js\nfunction sum (a, b) {\n   return a + b;\n}\nsum(1, 2);\n```");
+        })).toBe("```js\nfunction sum (a, b) {\n   return a + b;\n}\nsum(1, 2);\n```\n");
         cb();
     });
 
@@ -113,7 +116,7 @@ tester.describe("json2md", test => {
         };
         test.expect(json2md({
             sayHello: "World"
-        })).toBe("Hello World!")
+        })).toBe("Hello World!\n")
         cb();
     });
 
@@ -126,7 +129,10 @@ tester.describe("json2md", test => {
                         code: {
                             language: "js",
                             content: [
-                                "function sum (a, b) {", "   return a + b;", "}", "sum(1, 2);"
+                                "function sum (a, b) {",
+                                "   return a + b;",
+                                "}",
+                                "sum(1, 2);"
                             ]
                         }
                     }
@@ -139,7 +145,8 @@ tester.describe("json2md", test => {
        return a + b;
     }
     sum(1, 2);
-    \`\`\``);
+    \`\`\`
+    \n`);
         cb();
     });
 
@@ -164,7 +171,8 @@ tester.describe("json2md", test => {
        return a + b;
     }
     sum(1, 2);
-    \`\`\``);
+    \`\`\`
+    \n`);
         cb();
     });
 
@@ -184,7 +192,7 @@ tester.describe("json2md", test => {
                     b: "col2"
                 }]
             }
-        })).toBe("a | b\n--- | ---\ncol1 | col2");
+        })).toBe("a | b\n--- | ---\ncol1 | col2\n");
         cb();
     })
 
@@ -196,7 +204,7 @@ tester.describe("json2md", test => {
                     ["col1", "col2"]
                 ]
             }
-        })).toBe("a | b\n--- | ---\ncol1 | col2");
+        })).toBe("a | b\n--- | ---\ncol1 | col2\n");
         cb();
     })
 });
